f3e160d8155e401bf7261e3336d1a052b4fcc128
wangpeng <hustg4@163.com>
2014-08-09 13:01:50 +0800

    还原appdelegate

--- frameworks/runtime-src/Classes/AppDelegate.cpp
+++ frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -1,7 +1,13 @@
 #include "AppDelegate.h"
+#include "CCLuaEngine.h"
 #include "SimpleAudioEngine.h"
 #include "cocos2d.h"
 
+#include "framework.h"
+#include "HttpService.h"
+#include "SocketService.h"
+
+#include "lua_framework_auto.hpp"
 
 using namespace CocosDenshion;
 
@@ -26,33 +32,52 @@ bool AppDelegate::applicationDidFinishLaunching()
 		glview = GLView::createWithRect("framework", Rect(0,0,900,640));
 		director->setOpenGLView(glview);
 	}
-
+    
     glview->setDesignResolutionSize(480, 320, ResolutionPolicy::NO_BORDER);
-
+    
     // turn on display FPS
     director->setDisplayStats(true);
-
+    
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
-
     
+    //框架版本号
+    CCLOG("Framework: version:%.2f",FrameworkVersion);
+    
+    //配置SearchPath
+    ResourceManager::sharedResourceManager()->configSearchPath();
     
     //init network
     this->initNetwork();
-
-
+    
+	auto engine = LuaEngine::getInstance();
+	ScriptEngineManager::getInstance()->setScriptEngine(engine);
+    
+    lua_State* luaState = engine->getLuaStack()->getLuaState();
+    
+    register_all_framework(luaState);
+    
+    // run main lua file
+    engine->executeScriptFile("main.lua");
+    
     return true;
 }
 
 void AppDelegate::initNetwork()
 {
+    //添加NetService
+    NetCenter* netCenter=NetCenter::sharedNetCenter();
+    //   http
+    netCenter->addNetService("http", HttpService::create());
+    //   socket
+    netCenter->addNetService("socket", SocketService::create());
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground()
 {
     Director::getInstance()->stopAnimation();
-
+    
     SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
@@ -60,6 +85,6 @@ void AppDelegate::applicationDidEnterBackground()
 void AppDelegate::applicationWillEnterForeground()
 {
     Director::getInstance()->startAnimation();
-
+    
     SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
